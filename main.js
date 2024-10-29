06380941baaa3191bd7caf269baa9bbf46ca613a
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-29 09:58:07 +0100

    correction

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ console.log(process.argv);
 const pin = process.env.PIN;
 console.log(`il valore di PIN è: ${pin}`);
 
-const product = process.argv.slice(2)
+const product = process.argv[2]
 console.log(product);
 if (pin == "666" && product == "admin") {
     console.log("Welcome Admin")
